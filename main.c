0d72f3739ace4ab7673275e8c747aef783b5400d
Sheolis <59622118+Sheolis@users.noreply.github.com>
2020-01-13 12:10:21 +0100

    In main.c Removed function Initialisation (useless) Added player choice (att/def)

--- main.c
+++ main.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 
-int initialisation() {
-  return 30;
-}
 int main() {
-  int hp_player;
-  int hp_moob;
-  int dmg_sword=7;
-  int dmg_bite=10;
+  int hp_player=30;
+  int hp_moob=30;
+  int dmg_sword=8;
+  int dmg_bite=12;
+  int action_player;
+  int action_moob;
   int i; int j; int k;
 
-  hp_player=initialisation();
-  hp_moob=initialisation();
+
   while (hp_moob>0 && hp_player>0) {
-    printf("Basilic life points: %d\n",hp_moob);
-    printf("Sword slash inflict %d life points to the monster.\n",dmg_sword);
-    hp_moob-=dmg_sword;
-    printf("Basilic life points: %d\n",hp_moob);
+    printf("Attack{1} or defend{0} ?\n");
+    scanf("%d",&action_player);
+    if (action_player==1){
+      printf("Basilic life points: %d\n",hp_moob);
+      printf("Sword slash inflict %d life points to the monster.\n",dmg_sword);
+      hp_moob-=dmg_sword;
+      printf("Basilic life points: %d\n",hp_moob);
+    }
     printf("Your life points : %d\n",hp_player);
-    printf("The monster bite you, inflicting %d life points\n",dmg_bite);
-    hp_player-=dmg_bite;
+
+    if (action_player==0) {
+      printf("The monster bite you, inflicting %d life points\n",dmg_bite/4);
+      hp_player-=dmg_bite/4;
+    }
+    else {
+      printf("The monster bite you, inflicting %d life points\n",dmg_bite);
+      hp_player-=dmg_bite;
+    }
     printf("Your life points : %d\n",hp_player);
   }
-
   return 0;
 }
