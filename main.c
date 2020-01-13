bf3f39619911335a62a72521ff70bb524e78f1d4
Sheolis <59622118+Sheolis@users.noreply.github.com>
2020-01-13 11:53:27 +0100

    Updated main.c Added Bsilic's attack Exercise 3 complete

--- main.c
+++ main.c
@@ -6,16 +6,21 @@ int initialisation() {
 int main() {
   int hp_player;
   int hp_moob;
-  int dmg_sword=10;
+  int dmg_sword=7;
+  int dmg_bite=10;
   int i; int j; int k;
 
   hp_player=initialisation();
   hp_moob=initialisation();
-  while (hp_moob>0) {
-    printf("life points: %d\n",hp_moob);
+  while (hp_moob>0 && hp_player>0) {
+    printf("Basilic life points: %d\n",hp_moob);
     printf("Sword slash inflict %d life points to the monster.\n",dmg_sword);
     hp_moob-=dmg_sword;
-    printf("life points: %d\n",hp_moob);
+    printf("Basilic life points: %d\n",hp_moob);
+    printf("Your life points : %d\n",hp_player);
+    printf("The monster bite you, inflicting %d life points\n",dmg_bite);
+    hp_player-=dmg_bite;
+    printf("Your life points : %d\n",hp_player);
   }
 
   return 0;
